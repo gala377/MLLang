2e0f49314b66c080355967d7b73699c9775c9e06
Rafał Galczak <r.galczak@gmail.com>
2021-09-24 22:08:26 +0200

    Added some scratch of binary

---
+++ cmd/funk/main.go
@@ -0,0 +1,15 @@
+package funk
+
+// func EvaluateFile() {
+// 	filename := os.Args[1]
+// 	if buffer, err := ioutil.ReadFile(filename); err != nil {
+// 		fmt.Println(fmt.Errorf("%s file not found", filename))
+// 		os.Exit(-1)
+// 	} else {
+// 		//
+// 	}
+// }
+
+// func main() {
+
+// }
