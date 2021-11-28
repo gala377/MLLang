e94c518c82cb9df2a3cbef0b5d70edcc1fb070df
gala377 <r.galczak@gmail.com>
2021-11-28 16:38:43 +0100

    Made parsing test files async. No real advantage yet

--- tests/t.py
+++ tests/t.py
@@ -3,6 +3,7 @@ import subprocess
 import tempfile
 import re
 import argparse
+import trio
 
 from dataclasses import dataclass
 from pathlib import Path
@@ -33,6 +34,18 @@ def run(args):
         if not (fnk_binary.exists() and fnk_binary.is_file()):
             raise RuntimeError(f"Funk binary {fnk_binary} does not exist")
         run_tests(fnk_binary, test_files)
+
+async def run_async(args):
+    pat = args.filter[0] if args.filter else None
+    test_files = get_tests(tests_dir, pattern=pat)
+    print_tests(tests_dir, test_files)
+    with tempfile.TemporaryDirectory() as build_dir:
+        build_dir_path = Path(build_dir)
+        build_fnk(root_dir, build_dir_path)
+        fnk_binary = build_dir_path / FNK_BINARY_NAME
+        if not (fnk_binary.exists() and fnk_binary.is_file()):
+            raise RuntimeError(f"Funk binary {fnk_binary} does not exist")
+        await run_tests_async(fnk_binary, test_files)
 
 def get_tests(path: Path, pattern: Optional[str]) -> list[Path]:
     files = list(path.glob(f"**/*.{TEST_FILES_EXTENSION}"))
@@ -103,6 +116,46 @@ def run_tests(binary: Path, tests: list[Path]):
                 test_file.close()
     print_stats(failed, tests)
 
+async def run_tests_async(binary: Path, tests: list[Path]):
+    failed = []
+    for test in tests:
+        print(f"Running test {strip_tests_dir(test)}... ", end='')
+        test_path, expected = await parse_test_file_async(trio.Path(test.absolute()))
+        test_output = expected is not None
+        if test_output:
+            test_file = test_path
+            test_path = test_file.name
+        try:
+            proc = subprocess.run([binary, test_path], check=True, capture_output=True)
+            if test_output:
+                output = proc.stdout.splitlines()
+                output = list(map(lambda s: s.decode("utf-8"), output))
+                fail_msg = (
+                    "STDOUT DOES NOT MATCH:\nExpected: ==========\n\n" +
+                    "\n".join(expected) +
+                    "\nGOT: ===========\n\n" +
+                    "\n".join(output))
+                if len(expected) != len(output):
+                    raise TestFailed(test, fail_msg)
+                for e_line, a_line in zip(expected, output):
+                    if e_line != a_line:
+                        raise TestFailed(test, fail_msg)
+        except subprocess.CalledProcessError as e:
+            failed.append((
+                test,
+                e.stdout.decode("utf-8"),
+                e.stderr.decode("utf-8")))
+            print("Failed")
+        except TestFailed as e:
+            failed.append((test, e.msg, ""))
+            print("Failed")
+        else:
+            print("Passed")
+        finally:
+            if test_output:
+                await test_file.aclose()
+    print_stats(failed, tests)
+
 def parse_test_file(test: Path):
     with test.open() as tf:
         if (l := tf.readline()) != "@EXPECTED\n":
@@ -115,6 +168,22 @@ def parse_test_file(test: Path):
         test_source.writelines(map(lambda x: bytes(x, 'utf-8'), source))
         test_source.seek(0)
         test_source.flush()
+        expected = list(filter(bool, map(str.rstrip, expected)))
+        return test_source, expected
+
+async def parse_test_file_async(test: trio.Path):
+    async with await test.open() as tf:
+        if (l := await tf.readline()) != "@EXPECTED\n":
+            return test, None
+        expected = []
+        async for line in tf :
+            if line == "@SOURCE\n":
+                break
+            expected.append(line)
+        test_source = trio.wrap_file(tempfile.NamedTemporaryFile())
+        async for line in tf:
+            await test_source.write(bytes(line, 'utf-8'))
+        await test_source.seek(0)
         expected = list(filter(bool, map(str.rstrip, expected)))
         return test_source, expected
 
@@ -143,4 +212,4 @@ def command_parser():
 if __name__ == '__main__':
     parser = command_parser()
     args = parser.parse_args()
-    run(args)
+    trio.run(run_async, args)
